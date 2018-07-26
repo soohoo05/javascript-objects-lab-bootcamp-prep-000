ff09be2f7774f16f2368cb1b2f80c64021e4f104
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-26 20:47:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function deleteFromObjectByKey(object,key){
   delete newObj[key]
   return newObj
 }
+function destructivelyDelteFromObjectByKey(object,key){
+  delete object[key]
+  return object
+}
